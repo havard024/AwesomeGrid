504d68cae2d56fd96c201d8eb62bd0fc9b7d9ec8
Håvard <havard@ip-10-229-77-206.eu-west-1.compute.internal>
2013-10-07 09:53:33 +0200

    Initial commit

---
+++ js/controllers/TableCtrl.js
@@ -0,0 +1,29 @@
+var TableCtrl = function($scope) {
+
+	$scope.title = "hei";
+    var columns = [{
+	label : 'Label 1',
+	value : 'Value 1'
+    }, {
+	label : 'Label 2',
+	value : 'Value 2'
+    }, {
+	label : 'Label 3',
+	value : 'Value 3'
+    }, {
+	label : 'Label 4',
+	value : 'Value 4'
+    }, {
+	label : 'Label 5',
+	value : 'Value 5'
+    }, {
+	label : 'Label 6',
+	value : 'Value 6'
+    }];
+
+    $scope.columns = columns;
+}
+
+TableCtrl.$inject = ['$scope'];
+
+angular.module('App').controller('TableCtrl', TableCtrl);
